d8e24cf781b43dc38fec6842b68f88843950f3fd
Adam Shumann <ashumann13@gmail.com>
2018-06-21 08:14:45 -0500

    Path parsing tweaks

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { send, json } = require('micro')
 const axios = require('axios')
 const util = require('util')
 const JWT = require('jsonwebtoken')
+const { URL } = require('url')
 
 const verifyAsync = util.promisify(JWT.verify)
 
@@ -10,11 +11,23 @@ const backend = axios.create({ baseURL: process.env.BACKEND_URL })
 const JWT_ALGORITHM = process.env.JWT_ALGORITHM
 const JWT_PUBLIC = process.env.JWT_PUBLIC
 const JWT_SECRET = process.env.JWT_SECRET
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS
+
 
 module.exports = async (req, res) => {
-  if (req.url.match('/healthz')) return { healthy: true }
-  if (req.url.match('/find$')) return find(req, res)
-  if (req.url.match('/render$')) return render(req, res)
+  const { origin } = req.headers
+  const { host, pathName, searchParams } = new URL(req.url)
+  const allowed = ALLOWED_ORIGINS.toLowerCase().split(' ')
+  if (allowed.includes(origin.toLowerCase())) {
+    res.setHeader('access-control-allow-origin', origin)
+    res.setHeader('access-control-allow-methods', 'POST, GET')
+    res.setHeader('access-control-allow-headers', 'Content-Type, *')
+    res.setHeader('access-control-allow-credentials', 'true')
+  }
+  req.query = searchParams
+  if (pathName.match('/healthz')) return { healthy: true }
+  if (pathName.match('/find')) return find(req, res)
+  if (pathName.match('/render$')) return render(req, res)
   send(res, 404, { error: 'Not found' })
 }
 
